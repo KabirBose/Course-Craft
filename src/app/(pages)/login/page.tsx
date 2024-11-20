2d29262559e0c49e75fe3b28e8048a958454d9a0
Kabir Bose <kabirbose04@gmail.com>
2024-11-19 20:26:27 -0500

    /login page and form created

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -3,12 +3,9 @@ import Link from "next/link";
 export default function () {
   return (
     <div className="flex flex-col gap-5 justify-center items-center h-[100vh]">
-      <h1>Register</h1>
+      <h1>Login</h1>
 
       <form className="flex flex-col gap-5 justify-center items-center">
-        <input className="bg-gray-100" placeholder="First Name" type="text" />
-        <input className="bg-gray-100" placeholder="Last Name" type="text" />
-        <input className="bg-gray-100" placeholder="Student ID" type="number" />
         <input
           className="bg-gray-100"
           placeholder="Student Email"
@@ -17,7 +14,7 @@ export default function () {
         <input className="bg-gray-100" placeholder="Password" type="password" />
       </form>
 
-      <Link href="/login">Have an account? Login</Link>
+      <Link href="/register">No account? Register</Link>
     </div>
   );
 }
