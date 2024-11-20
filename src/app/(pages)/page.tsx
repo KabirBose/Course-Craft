91eb9436d1cf6c4f095b7861e0b0d1e04361c7e6
Kabir Bose <kabirbose04@gmail.com>
2024-11-19 20:17:11 -0500

    updated directory

---
+++ src/app/(pages)/page.tsx
@@ -0,0 +1,7 @@
+export default function () {
+  return (
+    <div className="flex justify-center items-center h-[100vh]">
+      <h1>Course Craft</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function () {
-  return <div>Course Craft</div>;
-}
