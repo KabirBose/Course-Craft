275fe999cf425cf966497f7dce6ddca8da7e2c2d
Kabir Bose <kabirbose04@gmail.com>
2024-11-20 21:39:04 -0500

    added calendar plugins

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -3,15 +3,18 @@
 import { ScheduleXCalendar, useCalendarApp } from "@schedule-x/react";
 import {
   CalendarApp,
-  createViewWeek,
   createViewMonthGrid,
   createViewDay,
+  createViewWeek,
 } from "@schedule-x/calendar";
 import "@schedule-x/theme-default/dist/calendar.css";
+import { createDragAndDropPlugin } from "@schedule-x/drag-and-drop";
+import { createEventModalPlugin } from "@schedule-x/event-modal";
+import { createResizePlugin } from "@schedule-x/resize";
 
 export default function Calendar() {
   const calendar: CalendarApp = useCalendarApp({
-    views: [createViewWeek(), createViewMonthGrid(), createViewDay()],
+    views: [createViewMonthGrid(), createViewDay(), createViewWeek()],
     events: [
       {
         id: Math.random(),
@@ -21,6 +24,11 @@ export default function Calendar() {
       },
     ],
     selectedDate: "2025-01-01",
+    plugins: [
+      createDragAndDropPlugin(),
+      createEventModalPlugin(),
+      createResizePlugin(),
+    ],
   });
 
   return (
